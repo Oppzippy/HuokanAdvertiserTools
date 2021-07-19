8edb16471e9d02dbbd746b70d44175a392dee443
Kyle Scheuing <kyle@kylescheuing.com>
2021-07-18 20:07:47 -0400

    Fix out of date message on login GetGuildInfo doesn't have data on PLAYER_ENTERING_WORLD after a login, only after a /reload. This will retry until it has data with a maximum number of tries.

--- Modules/NotInCommunityGuild.lua
+++ Modules/NotInCommunityGuild.lua
@@ -9,7 +9,18 @@ end
 
 function module:PLAYER_ENTERING_WORLD()
 	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	if not self:IsInCommunityGuild() then
-		Core:Print(L.warning_not_in_community_guild)
-	end
+	-- GetGuildInfo doesn't have data yet but IsInGuild does
+	-- Keep retrying until GetGuildInfo returns proper data
+	local timer
+	timer = C_Timer.NewTicker(1, function()
+		if not IsInGuild() then
+			Core:Print(L.warning_not_in_community_guild)
+			timer:Cancel()
+		elseif GetGuildInfo("player") then
+			timer:Cancel()
+			if not self:IsInCommunityGuild() then
+				Core:Print(L.warning_not_in_community_guild)
+			end
+		end
+	end, 30)
 end
